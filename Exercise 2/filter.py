9a4bb7e11c2cbd6f82d0a29ed6926b9bec27fbcd
erikhlp <domingueserikh@gmail.com>
2023-10-07 13:18:19 -0300

    Exercise 2 - Async

--- Exercise 2/filter.py
+++ Exercise 2/filter.py
@@ -1,54 +1,51 @@
-import requests
+import asyncio
+import aiohttp
 import json
 import pandas as pd
 
-# Define the API URL
-api_url = "https://restcountries.com/v3.1/all"
+async def fetch_data(api_url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(api_url) as response:
+            if response.status == 200:
+                data = await response.json()
+                return data
+            else:
+                return None
 
-# Make a GET request to the API
-response = requests.get(api_url)
+async def main():
+    api_url = "https://restcountries.com/v3.1/all"
+    data = await fetch_data(api_url)
 
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    data = json.loads(response.text)
-    
-    # Initialize empty lists for each field
-    names = []
-    currency_names = []
-    populations = []
-    
-    for country in data:
-        region = country.get("region", None)
-        subregion = country.get("subregion", None)
-        
-        # Check if the country is in Northern Europe
-        if region == "Europe" and subregion == "Northern Europe":
-            # Extract the desired fields
-            name = country.get("name", {}).get("official", None)
-            currencies = country.get("currencies", None)
-            population = country.get("population", None)
+    if data:
+        names = []
+        currency_names = []
+        populations = []
 
-            # Check if all desired fields are available
-            if name and currencies and population is not None:
-                names.append(name)
-                
-                # Extract the "name" key from the nested dictionary "currencies"
-                # currency_name = currencies.get("0", {}).get("1", {}).get("name")
-                currency_name = next(iter(currencies.values()))['name']
-                currency_names.append(currency_name)
+        for country in data:
+            region = country.get("region", None)
+            subregion = country.get("subregion", None)
 
-                
-                
-                populations.append(population)
+            if region == "Europe" and subregion == "Northern Europe":
+                name = country.get("name", {}).get("official", None)
+                currencies = country.get("currencies", None)
+                population = country.get("population", None)
 
-    # Create a Pandas DataFrame with the three columns
-    df = pd.DataFrame({
-        "nation_official_name": names,
-        "currency_name": currency_names,
-        "population": populations
-    })
-    
-    # Display the DataFrame
-    print(df)
-else:
-    print("Failed to retrieve data from the API.")
+                if name and currencies and population is not None:
+                    names.append(name)
+                    currency_name = next(iter(currencies.values()))['name']
+                    currency_names.append(currency_name)
+                    populations.append(population)
+
+        df = pd.DataFrame({
+            "nation_official_name": names,
+            "currency_name": currency_names,
+            "population": populations
+        })
+
+        print(df)
+    else:
+        print("Failed to retrieve data from the API.")
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
